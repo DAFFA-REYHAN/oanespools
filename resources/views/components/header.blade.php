3bba3a141259c5b4636741975d7ef685755984da
ParkMuJon <jonwork.c@gmail.com>
2025-08-20 14:50:43 +0700

    Add : WhatsApp direct link

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -4,10 +4,14 @@
         <span class="text-blue-700 font-bold text-2xl italic">Oanes Pools</span>
     </div>
     @auth
-    <a href="{{route('dashboard')}}" class="inline-block px-6 py-3 bg-blue-600 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50">
-    Dashboard
-    </a>
+        <a href="{{ route('dashboard') }}"
+            class="inline-block px-6 py-3 bg-blue-600 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50">
+            Dashboard
+        </a>
     @endauth
-    <a href="#"
-        class="font-semibold bg-blue-700 text-white px-5 py-3 rounded-full text-sm hover:bg-blue-800">Hubungi Kami</a>
+    <a href="https://wa.me/6281291100645" target="_blank"
+        class="font-semibold bg-blue-700 text-white px-5 py-3 rounded-full text-sm hover:bg-blue-800">
+        Hubungi Kami
+    </a>
+
 </nav>
